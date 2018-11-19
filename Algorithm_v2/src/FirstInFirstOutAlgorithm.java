a4096056e26e372cc0afbee78e6471d06b05edef
Juan Chavez <juanchavez017@gmail.com>
2018-11-19 00:23:54 -0800

    Implemented first in first out

--- Algorithm_v2/src/FirstInFirstOutAlgorithm.java
+++ Algorithm_v2/src/FirstInFirstOutAlgorithm.java
@@ -5,15 +5,35 @@ public class FirstInFirstOutAlgorithm implements IAlgorithm {
 	@Override
 	public ICar findBestCar(List<ICar> lstCars, Direction direction, int destinationFloorNumber) {
 		// TODO Auto-generated method stub
-		ICar firstCar = null;
+		ICar bestCar = null;
+		
 		if(lstCars.size() == 1) {
-			firstCar = lstCars.get(0);
+			//Only one car, so this is the best car
+			bestCar = lstCars.get(0);
+			return bestCar;
 		}
-		else if (lstCars.size() > 1) {
-			firstCar = lstCars.remove(0);
-			lstCars.add(firstCar);
+		
+		for(int i = 0; i < lstCars.size()-1; i++) {
+			ICar currentCar = lstCars.get(i);
+			int carFloor = currentCar.getCurrentFloorNumber();
+			CarStatus currentStatus = currentCar.getStatus();
+			
+			//The best car for the user would be the one going toward their destination floor number
+			if(destinationFloorNumber > carFloor) {
+				if(currentStatus == CarStatus.MOVING_UP) {
+					bestCar = currentCar;
+				}
+			}
+			if(destinationFloorNumber < carFloor) {
+				if(currentStatus == CarStatus.MOVING_DOWN) {
+					bestCar = currentCar;
+				}
+			}
+			System.out.print("Current best car" + bestCar.getCarID());
+			//If neither condition is satisfied, then were on the same floor were trying to go too
 		}
-		return firstCar;
+		
+		return bestCar;
 	}
 
 }
